90a1a99a673080f0341e42167be43de55a332b39
MiikkaR7 <miikkariipi22@gmail.com>
2024-10-21 13:52:56 +0300

    feat: sidebar navigation to existing skeleton pages

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,6 +6,22 @@ function Sidebar() {
     <>
     <div className="sidebar">
       <h1>Twitter clone</h1>
+      <nav>
+          <ul>
+          <li>
+            <a href={`/`}>Home</a>
+          </li>
+          <li>
+            <a href={`/explore`}>Explore</a>
+          </li>
+          </ul>
+
+          <ul>
+          <li>
+            <a href={`/user/example`}>Profile</a>
+          </li>
+          </ul>
+        </nav>
     </div>
     </>
   )
