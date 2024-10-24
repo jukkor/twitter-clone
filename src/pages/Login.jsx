190b7ec50d080d1908bb258754a5a2e7be0c7c36
MiikkaR7 <miikkariipi22@gmail.com>
2024-10-24 14:32:38 +0300

    feat:began work on login page, styling it according to figma document

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,11 +1,19 @@
-import './../App.css'
+import './Login.css'
 
 function Login() {
 
   return (
     <>
-    <div>
-      <h1>Login/sign up page</h1>
+    <div className="logo-text">
+      <h1>Twitter clone</h1>
+    </div>
+    <div className="auth-options">
+      <div className="auth-buttons">
+        <button>Sign in with Google</button>
+        <button>Sign in with Facebook</button>
+        <button>Sign in with Email</button>
+        <button>Sign in with Phone</button>
+      </div>
     </div>
     </>
   )
