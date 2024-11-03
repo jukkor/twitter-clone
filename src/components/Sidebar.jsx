845e889fe553e5752d26592f9dd83fbf9696356f
Jukka Korhonen <jukka.korhonen@outlook.com>
2024-11-03 13:24:51 +0200

    feat: sign out button in sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,28 +1,40 @@
+import { useNavigate } from 'react-router-dom';
+import { auth } from '../firebase/firebase';
 import './Sidebar.css'
 
 function Sidebar() {
 
+  const navigate = useNavigate();
+
+  const handleSignOut = () => {
+    auth.signOut();
+    navigate("/login");
+  }
+
   return (
     <>
-    <div className="sidebar">
-      <h1>Twitter clone</h1>
-      <nav>
+      <div className="sidebar">
+        <h1>Twitter clone</h1>
+        <nav>
           <ul>
-          <li>
-            <a href={`/`}>Home</a>
-          </li>
-          <li>
-            <a href={`/explore`}>Explore</a>
-          </li>
+            <li>
+              <a href={`/home`}>Home</a>
+            </li>
+            <li>
+              <a href={`/explore`}>Explore</a>
+            </li>
           </ul>
 
           <ul>
-          <li>
-            <a href={`/user/example`}>Profile</a>
-          </li>
+            <li>
+              <a href={`/user/example`}>Profile</a>
+            </li>
           </ul>
         </nav>
-    </div>
+        <div>
+          <a onClick={handleSignOut} >Sign Out</a>
+        </div>
+      </div>
     </>
   )
 }
