53e34d13042cfbc5b6b82bdb780037a14d966e50
MiikkaR7 <miikkariipi22@gmail.com>
2024-12-03 13:22:28 +0200

    feat: profile picture shown in tweet, get displayName and photoURL using tweets userId

--- src/pages/ExplorePage.jsx
+++ src/pages/ExplorePage.jsx
@@ -1,34 +1,36 @@
 import React, { useState, useEffect } from 'react';
-
-import { ref, onValue } from 'firebase/database';
+import { ref, onValue, get } from 'firebase/database';
 import { db } from '../firebase/firebase';
 
 import Sidebar from '../components/Sidebar';
 import TweetList from '../components/TweetList';
 import { useUser } from '../contexts/UserContext';
 
-import examplePfp from '../assets/google.png';
-
 function ExplorePage() {
   const [tweets, setTweets] = useState([]);
-
   const { user } = useUser();
-  console.log(user);
-
-  console.log()
 
   useEffect(() => {
     const tweetsRef = ref(db, 'tweets');
 
-    const unsubscribe = onValue(tweetsRef, (snapshot) => {
+    const unsubscribe = onValue(tweetsRef, async (snapshot) => {
       const data = snapshot.val();
       if (data) {
-        const tweetsArray = Object.entries(data).map(([key, value]) => ({
-          id: key,
-          username: value.displayName,
-          profilePicture: examplePfp,
-          content: value.content
-        }));
+        const tweetsArray = await Promise.all(
+          Object.entries(data).map(async ([key, value]) => {
+            const userRef = ref(db, `users/${value.userId}`);
+            const userSnapshot = await get(userRef);
+            const userProfile = userSnapshot.val();
+
+            return {
+              id: key,
+              username: userProfile?.displayName || null,
+              profilePicture: userProfile?.photoURL || null,
+              content: value.content,
+            };
+          })
+        );
+
         setTweets(tweetsArray);
       } else {
         setTweets([]);
